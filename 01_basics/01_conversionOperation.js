866535fbae0a966a5185d595ef853d4a55ead9ab
Muhammad Haris <raoh0163@gmail.com>
2025-02-19 09:25:07 +0000

    conversion of datatypes

---
+++ 01_basics/01_conversionOperation.js
@@ -0,0 +1,26 @@
+let score = "Haris"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+
+console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1;false => 0;
+
+let isloggedIn = Symbol
+ 
+let booleanisloggedIn = Boolean(isloggedIn);
+console.log( booleanisloggedIn);
+
+// "" => false 
+// "Haris" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+
+console.log( stringNumber);
