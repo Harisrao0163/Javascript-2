3321998a0fa6fc01e0853e0092b2e681755fc3f3
Muhammad Haris <raoh0163@gmail.com>
2025-02-22 21:12:28 +0000

    datatypes summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,30 @@
+// primitive type...
+// string,Number,Boolean,bigint,symbol,null,undefined.
+
+const score= 100
+const scorevalue= 100.3
+
+const isloggedin= false
+let userEmail;
+const outsidetemp=null
+ 
+const id= Symbol('123');
+const anotherid= Symbol('123');
+
+// console.log( id === anotherid);
+
+const bigNumber= 42346432342354321436n
+
+// reference type (non-primitive type)....
+// Array,Objects,Functions.
+const names=["haris","owais","Ahmad"]
+let myobj={
+    name:"haris",
+    age: 22
+}
+const myFunction = function(){
+    console.log("Hello World"); 
+}
+console.log(typeof outsidetemp);
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
