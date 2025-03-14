bf56f42440d1b1c70775b356370fcd443850f47f
Muhammad Haris <raoh0163@gmail.com>
2025-03-14 20:16:12 +0000

    if else condition

---
+++ 04_control_flow/one.js
@@ -0,0 +1,55 @@
+const temperature = 60
+
+// if (temperature < 50){
+//       console.log("true");
+// } else {
+//     console.log(false);
+    
+// }
+// console.log("yes,true");
+
+const score = 200
+
+// if(score > 100){
+//       var power ="fly" 
+//       console.log(`user power: ${power}`);
+      
+// }
+// console.log(`user power: ${power}`);// now it shows bcz, var defines as global...
+
+// if(score > 100){
+//     let power ="fly" 
+//     console.log(`user power: ${power}`);
+    
+// }
+// console.log(`user power: ${power}`);// now, it does not defines the outer value of log bcz of let/const.....
+
+const balance = 1000
+
+// if (balance > 500) console.log("true");// this another method to express the if value....this is also known as implicit scope...
+
+
+// if (balance < 500) {
+//     console.log("true");
+// } else if(balance  < 750){
+//     console.log("true");              // this all code is for multiple checking.....
+// } else if(balance < 900){
+// } else{
+//     console.log("equal to 1000");
+// }
+
+// real life example............
+
+const userloggedIn = true
+const debitcard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+if (userloggedIn && debitcard ) {
+    console.log("Allow to Access");
+}
+
+if (loggedInFromEmail || loggedInFromGoogle) {
+    console.log("user logged in");
+    
+}
